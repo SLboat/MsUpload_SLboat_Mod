6f10b9203be7a75599db8bed9f41316109a93943
kik888@gmail.com <kik888@gmail.com@0628eb2e-1676-5079-c15c-92e911198d8e>
2013-04-20 16:16:18 +0000

    * 更新到plupload V1.5.7版本

--- js/plupload/i18n/sr.js
+++ js/plupload/i18n/sr.js
@@ -1,4 +1,4 @@
-// Serbian
+﻿// Serbian
 plupload.addI18n({
     'Select files' : 'Izaberite fajlove',
     'Add files to the upload queue and click the start button.' : 'Dodajte fajlove u listu i kliknite na dugme Start.',
