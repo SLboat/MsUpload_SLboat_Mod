7a39b8e935175c908c490ed145499d7bfaa4fadd
kik888@gmail.com <kik888@gmail.com@0628eb2e-1676-5079-c15c-92e911198d8e>
2013-04-20 17:20:09 +0000

    再一个换行

--- js/msupload.js
+++ js/msupload.js
@@ -274,7 +274,6 @@ function createUpload(wikiEditor){
         			
         		}).appendTo(file.li);// SLBoat: 创建插入影片按钮
 
-				}
         	}else{ // SLBoat: 其它文件也有个插入文件	
 				// SLBoat: 插入一根竖线
 				$(document.createElement("span")).text(' | ').appendTo(file.li); // SLBoat: 这是电影的特别玩意，改变图标文字
@@ -287,7 +286,6 @@ function createUpload(wikiEditor){
 
         	}// SLBoat: 下面还有一个换行是的
         	}//else error
-			//todo：生成一个有趣的music插入提示文本
         	
         }catch(e){//try
 			
