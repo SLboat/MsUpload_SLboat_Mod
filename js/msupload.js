384a0e62ef1e58b4569cbc93b78afa0ad876d871
Sen <slboat@me.com>
2013-10-25 22:10:40 +0800

    修正了一个问题-在ios里工作不正常的问题

--- js/msupload.js
+++ js/msupload.js
@@ -1,11 +1,17 @@
-var gallery_arr = new Array();
+var gallery_arr = new Array(); /* 全局的插入数组呢 */
 
 $(document).ready(function () { //jquery      
 /* Check if we are in edit mode and the required modules are available and then customize the toolbar */
 if ( $.inArray( mw.config.get( 'wgAction' ), ['edit', 'submit'] ) !== -1 ) {	//注入工具栏
-	mw.loader.using( 'user.options', function () {
-		if ( mw.user.options.get('usebetatoolbar') ) {
-	    	mw.loader.using( 'ext.wikiEditor.toolbar', function () { createUpload(true); });	//创建现代工具栏按钮
+	mw.loader.using( 'user.options', function () {		
+		if ( mw.user.options.get('usebetatoolbar') ) {			
+	    	mw.loader.using( 'ext.wikiEditor.toolbar', function () {
+	    		if ($.wikiEditor.isSupported($.wikiEditor.modules.toolbar)){ /* 在IOS它可能不会工作!即便设置了工作 */
+	    	 		createUpload(true); //创建现代工具栏按钮
+	    	 	}else{
+	    	 		createUpload(false); //fallback回到旧工作栏
+	    	 	}
+	    	 });
 		}else{ createUpload(false); }	//创建经典工具栏
 	});
 }
